69e6a533e9923cf129e6e60d0ed64287d5c65643
YashKachhiyaPatel <lifeline242734@gmail.com>
2021-06-16 20:45:31 -0700

    fixing routes

--- Server/Routes/contacting.js
+++ Server/Routes/contacting.js
@@ -8,9 +8,9 @@ const contacting_1 = require("../Controllers/contacting");
 const router = express_1.default.Router();
 exports.default = router;
 router.get('/', contacting_1.DisplayContactingListPage);
-router.get('/cedit/:id', contacting_1.DisplayContactingEditPage);
-router.get('/cadd', contacting_1.DisplayContactAddPage);
-router.post('/cadd', contacting_1.ProcessContactAddPage);
-router.post('/cedit/:id', contacting_1.ProcessContactEditPage);
-router.get('/cdelete/:id', contacting_1.ProcessContactDeletePage);
+router.get('/edit/:id', contacting_1.DisplayContactingEditPage);
+router.get('/add', contacting_1.DisplayContactAddPage);
+router.post('/add', contacting_1.ProcessContactAddPage);
+router.post('/edit/:id', contacting_1.ProcessContactEditPage);
+router.get('/delete/:id', contacting_1.ProcessContactDeletePage);
 //# sourceMappingURL=contacting.js.map
--- Server/Routes/contacting.ts
+++ Server/Routes/contacting.ts
@@ -6,17 +6,17 @@ export default router;
 
 router.get('/', DisplayContactingListPage);
 
-router.get('/cedit/:id', DisplayContactingEditPage);
+router.get('/edit/:id', DisplayContactingEditPage);
 
 /* GET - display /clothing-list/add page. */
-router.get('/cadd', DisplayContactAddPage);
+router.get('/add', DisplayContactAddPage);
 
 
 /* POST - process /clothing-list/add page */
-router.post('/cadd', ProcessContactAddPage);
+router.post('/add', ProcessContactAddPage);
 
 /* POST - process /clothing-list/edit/:id page */
-router.post('/cedit/:id', ProcessContactEditPage);
+router.post('/edit/:id', ProcessContactEditPage);
 
 /* GET - process /clothing-list/delete/:id */
-router.get('/cdelete/:id', ProcessContactDeletePage);
+router.get('/delete/:id', ProcessContactDeletePage);
