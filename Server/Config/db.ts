bf951bda133769d19a437ea6bd22511044acbba4
YashKachhiyaPatel <lifeline242734@gmail.com>
2021-06-16 21:22:20 -0700

    fixing errors to get list

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,3 +1,3 @@
-export const LocalURI = "mongodb://localhost/clothing_store";
+export const LocalURI = "mongodb://localhost/contacting_store";
 export const HostName = "localhost";
 export const Secret = "someSecret";
