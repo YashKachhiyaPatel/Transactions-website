7e36c90aad9a75ac8dcd1a36550616be4c93e14a
yashpatel1902 <kachhiyapatel1902@gmail.com>
2021-11-19 23:06:48 -0500

    mongodb remote added

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,6 +1,6 @@
 //user RemoteURI for mongoDb Atlas to use database remotely
 
 export const LocalURI = "mongodb://localhost/business_store";
-export const RemoteURI = "mongodb+srv://admin:admin@cluster-a2.f0wjo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-export const HostName = "Localhost";
+export const RemoteURI = "mongodb+srv://admin:a@123@clustertransaction.53mt9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const HostName = "RemoteHost";
 export const Secret = "someSecret";
